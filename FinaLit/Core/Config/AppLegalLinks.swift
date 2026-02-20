fb990c8c4817650ea36e7c90ce5e73bce5255437
Hakim Aliyev <115820565+hakim-cyber@users.noreply.github.com>
2026-02-21 02:20:01 +0400

    added prvacy policy and terms

--- FinaLit/Core/Config/AppLegalLinks.swift
+++ FinaLit/Core/Config/AppLegalLinks.swift
@@ -8,6 +8,6 @@
 import Foundation
 
 enum AppLegalLinks {
-    static let privacyPolicyURL = URL(string: "https://finalit.app/privacy")
-    static let termsOfUseURL = URL(string: "https://finalit.app/terms")
+    static let privacyPolicyURL = URL(string: "https://doc-hosting.flycricket.io/finalit-privacy-policy/65ec5e34-e1ec-44db-8d4e-07164dab5db7/privacy")
+    static let termsOfUseURL = URL(string: "https://doc-hosting.flycricket.io/finalit-terms-of-use/3821d11e-375a-4667-ba1b-e3fa67ef8a8f/terms")
 }
